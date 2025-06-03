45ef099ba351c9501d55ac843d20f6204333f389
Mark Fleschler <mfleschler@zoox.com>
2025-06-03 11:34:25 -0700

    Added gitignore; updated install.sh to cleanly replace sym links

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+
+create_symlink() {
+  local source_file="$1"
+  local dest_link="$2"
+  if [ -e "$dest_link" ] || [ -L "$dest_link" ]; then
+    rm -rf "$dest_link"
+  fi
+  ln -s "$source_file" "$dest_link"
+  echo "Created symlink: $dest_link -> $source_file"
+}
+
 echo "Creating symbolic links in home directory! Warning: will overwrite any existing files!"
 DOTFILE_PATH="$(pwd)"
 
-ln -s $DOTFILE_PATH/.bash_alias ~/.bash_alias
-ln -s $DOTFILE_PATH/.bash_func ~/.bash_func
-ln -s $DOTFILE_PATH/.bash_profile ~/.bash_profile
-ln -s $DOTFILE_PATH/.bashrc ~/.bashrc
-ln -s $DOTFILE_PATH/.profile ~/.profile
-ln -s $DOTFILE_PATH/.zshrc ~/.zshrc
-ln -s $DOTFILE_PATH/.p10k.zsh ~/.p10k.zsh
-ln -s $DOTFILE_PATH/.aerospace.toml ~/.aerospace.toml
+create_symlink "$DOTFILE_PATH"/.bash_alias ~/.bash_alias
+create_symlink "$DOTFILE_PATH"/.bash_func ~/.bash_func
+create_symlink "$DOTFILE_PATH"/.bash_profile ~/.bash_profile
+create_symlink "$DOTFILE_PATH"/.bashrc ~/.bashrc
+create_symlink "$DOTFILE_PATH"/.profile ~/.profile
+create_symlink "$DOTFILE_PATH"/.zshrc ~/.zshrc
+create_symlink "$DOTFILE_PATH"/.p10k.zsh ~/.p10k.zsh
+create_symlink "$DOTFILE_PATH"/.aerospace.toml ~/.aerospace.toml
 
 #echo "Installing oh-my-zsh"
 #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -19,4 +30,3 @@ ln -s $DOTFILE_PATH/.aerospace.toml ~/.aerospace.toml
 
 #echo "Installed. Be sure to run 'p10k configure' once everything is up"
 
-source ~/.profile
